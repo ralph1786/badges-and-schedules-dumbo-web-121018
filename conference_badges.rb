eab0c0de30e6918fd65f75d292febf689c53de87
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 00:38:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,6 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You will be assigned to "
+    "Hello, #{attendee}! You will be assigned to room #{index + 1}"
+  end
 end
