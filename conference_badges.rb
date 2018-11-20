574c7a8ef3148e71f2f4a8c7cb69edd357586e5e
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 00:34:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,5 @@ end
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-    
+    "Hello, my name is #{attendee}."
 end
