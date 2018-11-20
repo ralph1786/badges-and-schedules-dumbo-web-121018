9223321f00f6057572e7442fcbf1c94a805393c1
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 00:46:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,11 +16,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-  end
-
   assign_rooms(attendees).each do |assignment|
     puts assignment
+  end
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
 end
