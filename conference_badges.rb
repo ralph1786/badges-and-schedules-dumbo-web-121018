d15fbcf4509fbd4b2408fa0ea7c8619897cef02a
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 00:32:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-attendees = []
+attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 def batch_badge_creator(attendees)
   
 end
